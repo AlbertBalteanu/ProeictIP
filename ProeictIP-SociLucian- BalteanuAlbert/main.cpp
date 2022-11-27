beb557f5b5ec5a06bcaf56b6611597ab69b56fcc
unknown <lucian.soci@info.uaic.ro>
2022-11-27 16:49:35 +0200

    gata

--- ProeictIP-SociLucian- BalteanuAlbert/main.cpp
+++ ProeictIP-SociLucian- BalteanuAlbert/main.cpp
@@ -5,14 +5,29 @@
 #include <conio.h>
 
 using namespace std;
-int triunghi[] = {190,256,384,258,288,116,190,256,70,419,500,419,385,256};
-int casa[] = {190,256,384,258,288,116,190,256,190,419,385,419,385,256};
+int triunghi[] = {190,256, 384,258, 288,116, 190,256, 70,419, 500,419, 385,256};
+int casa[] = {190,256, 384,258, 288,116, 190,256, 190,419, 385,419, 385,256};
 void drawpoly( int number, int *polypoints );
+
+void morph(int *img1, int *img2){
+    drawpoly(7,img1);
+
+}
+
 int main()
 {
      initwindow(600,600);
-    // circle(250,250,100);
-    drawpoly(7,casa);
+
+     drawpoly(7,casa);
+
+     while(triunghi[8]!=casa[8]){ // while -> subprogram (cord img1, cord img 2)
+        delay(600);
+        clearviewport();
+        casa[8]=casa[8]-30; //diferenta calculata in notepad -> algoritm de calcul
+        casa[10]=casa[10]+28;
+        drawpoly(7,casa);
+     }
+
      getch();
      closegraph();
 
