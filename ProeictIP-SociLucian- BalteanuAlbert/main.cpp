e64e0fbf5cdb445005d0bf14f766bf5612877042
Albert Bălteanu <albert.balteanu@info.uaic.ro>
2022-11-27 16:28:26 +0200

    asd

--- ProeictIP-SociLucian- BalteanuAlbert/main.cpp
+++ ProeictIP-SociLucian- BalteanuAlbert/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include <winbgim.h>
+#include <graphics.h>
+#include <math.h>
+#include <conio.h>
 
 using namespace std;
-
+int triunghi[] = {190,256,384,258,288,116,190,256,70,419,500,419,385,256};
+int casa[] = {190,256,384,258,288,116,190,256,190,419,385,419,385,256};
+void drawpoly( int number, int *polypoints );
 int main()
 {
-    cout << "il folosim pe asta de acum" << endl;
+     initwindow(600,600);
+    // circle(250,250,100);
+    drawpoly(7,casa);
+     getch();
+     closegraph();
+
     return 0;
 }
