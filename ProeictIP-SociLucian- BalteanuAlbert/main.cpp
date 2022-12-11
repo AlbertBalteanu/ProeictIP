3416aa0de6ac71261c7846d660c0938240570567
Albert Bălteanu <albert.balteanu@info.uaic.ro>
2022-12-11 19:21:10 +0200

    aa

--- ProeictIP-SociLucian- BalteanuAlbert/main.cpp
+++ ProeictIP-SociLucian- BalteanuAlbert/main.cpp
@@ -42,7 +42,7 @@ void deseneazaMeniul()
     setcolor(WHITE);
     setfillstyle(SOLID_FILL,LIGHTGREEN);
 
-    line (0,80 , 1400,80); //linia orizontala
+    line (0,80, 1400,80);  //linia orizontala
     line (700,80, 700,700); // linia verticala
     line (0,250, 700,250);
 
@@ -56,11 +56,11 @@ void deseneazaMeniul()
 
     strcpy(B[i].text,"Alege imaginea 1");
     bar(B[i].D.SS.x, B[i].D.SS.y, B[i].D.DJ.x, B[i].D.DJ.y);
-        outtextxy(B[i].D.SS.x+10,B[i].D.SS.y+15,B[i].text);
+    outtextxy(B[i].D.SS.x+10,B[i].D.SS.y+15,B[i].text);
     setcolor(GREEN);
     rectangle(B[i].D.SS.x, B[i].D.SS.y,B[i].D.DJ.x,B[i].D.DJ.y);
 
-     for (i=2; i<=4; i++)
+    for (i=2; i<=4; i++)
     {
         setcolor(WHITE);
 
@@ -71,12 +71,12 @@ void deseneazaMeniul()
         B[i].D.DJ.y=200;
 
         if(i==2)strcpy(B[i].text,"Alege imaginea 2");
-            else if (i==3)strcpy(B[i].text,"START");
-                else if (i==4) strcpy(B[i].text,"Iesire");
+        else if (i==3)strcpy(B[i].text,"START");
+        else if (i==4) strcpy(B[i].text,"Iesire");
 
         bar(B[i].D.SS.x, B[i].D.SS.y, B[i].D.DJ.x, B[i].D.DJ.y);
         setbkcolor(BLACK);
-            outtextxy(B[i].D.SS.x+10,B[i].D.SS.y+15,B[i].text);
+        outtextxy(B[i].D.SS.x+10,B[i].D.SS.y+15,B[i].text);
         setcolor(GREEN);
         rectangle(B[i].D.SS.x, B[i].D.SS.y,B[i].D.DJ.x,B[i].D.DJ.y);
     }
@@ -94,8 +94,10 @@ void deseneazaMeniul()
 
     rectangle(B[i].D.SS.x, B[i].D.SS.y,B[i].D.DJ.x,B[i].D.DJ.y);//creeaza primul patrat/buton imagine ca referinta
 
-    for(i=6;i<=nrButoane;i++){
-        if(i==9){// schimbarea de nivel dupa 4 butoane imagine
+    for(i=6; i<=nrButoane; i++)
+    {
+        if(i==9) // schimbarea de nivel dupa 4 butoane imagine
+        {
             nivel=500;
             B[i].D.SS.x=13;
             B[i].D.SS.y=nivel;
@@ -103,13 +105,14 @@ void deseneazaMeniul()
             B[i].D.DJ.x=13+150;
             B[i].D.DJ.y=nivel+150;
         }
-            else{
-                B[i].D.SS.x=B[i-1].D.SS.x + 175 ;
-                B[i].D.SS.y=nivel;
+        else
+        {
+            B[i].D.SS.x=B[i-1].D.SS.x + 175 ;
+            B[i].D.SS.y=nivel;
 
-                B[i].D.DJ.x=B[i-1].D.DJ.x + 175;
-                B[i].D.DJ.y=nivel+150;
-                }
+            B[i].D.DJ.x=B[i-1].D.DJ.x + 175;
+            B[i].D.DJ.y=nivel+150;
+        }
 
         rectangle(B[i].D.SS.x, B[i].D.SS.y,B[i].D.DJ.x,B[i].D.DJ.y);
 
@@ -139,64 +142,167 @@ int butonAles()
     return 0;
 }
 
-//int trapez[] = {190,256, 384,256, 288,256, 190,256, 70,419, 500,419, 385,256};
-//int casa[] = {190,256, 384,256, 288,116, 190,256, 190,419, 385,419, 385,256};
-float trapez[15],casa[15];
-int trapezint[15],casaint[15];
+float img[8][50];
+int imgint[8][50];
 
-struct ecuatii
+/*struct ecuatii
 {
     float panta;
     float terlib; ///"termenul liber"
     int coefy;
-} ec[8];
-
-//void drawpoly( int number, int *polypoints );
+} ec[24];
+*/
 
 void calculeazaEcuatia(int x1, int y1, int x2, int y2,float &panta, float &terlib, int &coefy, int &lafel)
 {
     if(x1==x2&&y1==y2)
     {
         lafel=1;
-    }
-    else if(x1==x2)
-    {
-        coefy=0;
-        panta=-1;
-        terlib=x1;
-        lafel=2;
-    }
-    else if(y1==y2)
-    {
-        panta=0;
-        terlib=y1;
-        coefy=-1;
-        lafel=3;
-
+        afisecuatii<<"fara"<<endl;
     }
     else
     {
-        lafel=4;
-        panta=(y1-y2)/(x1-x2);
-        terlib=y1-(panta*x1);
-        coefy=1;
+        if(x1==x2)
+        {
+            coefy=0;
+            panta=1;
+            terlib=x1;
+            lafel=2;
+            afisecuatii<<"coefy "<<coefy<<" panta "<<panta<<" terlib "<<terlib<<endl;
+        }
+        else
+        {
+            if(y1==y2)
+            {
+                panta=0;
+                terlib=y1;
+                coefy=1;
+                lafel=3;
+                afisecuatii<<"coefy "<<coefy<<" panta "<<panta<<" terlib "<<terlib<<endl;
+
+            }
+            else
+            {
+                lafel=4;
+                panta=(((float)y2-(float)y1)/((float)x1-(float)x2));
+                terlib=(-(float)y1-(panta*(float)x1));
+                coefy=1;
+                afisecuatii<<"coefy "<<coefy<<" panta "<<panta<<" terlib "<<terlib<<endl;
+            }
+        }
     }
 }
-float distante[8];
-int laFel[8]; ///1=punctul nu se misca 2=pe verticala 3=pe orizontala 4=avem ecuatie cu toti coeficientii
-int stdr[8];///-1 daca merge in st sau jos, "1" daca merge sus sau dreapta
 
 
-    void culoareoriginala(int c){ // folosit pt aducere butoanele 1 si 2 la normal
-            setfillstyle(SOLID_FILL,LIGHTGREEN);
-            bar(B[c].D.SS.x, B[c].D.SS.y, B[c].D.DJ.x, B[c].D.DJ.y);
-            setcolor(WHITE);
-            setbkcolor(BLACK);
-            outtextxy(B[c].D.SS.x+10,B[c].D.SS.y+15,B[c].text);
-            setcolor(GREEN);
-            rectangle(B[c].D.SS.x, B[c].D.SS.y,B[c].D.DJ.x,B[c].D.DJ.y);
-            setcolor(WHITE);
+void morph(float a[],float b[],int aint[], int bint[],int n)
+{
+    int laFel[n]= {0}; ///1=punctul nu se misca 2=pe verticala 3=pe orizontala 4=avem ecuatie cu toti coeficientii
+
+    int stdr[n]= {0},susjos[n]= {0}; ///"0" daca merge in st sau jos, "1" daca merge sus sau dreapta
+
+    float distante[n]= {0};
+
+    struct ecuatii
+    {
+        float panta;
+        float terlib; ///"termenul liber"
+        int coefy;
+    } ec[25]= {0};
+
+    for(int i=0; i<n; i++) ///am mutat forul asta in fucntie
+    {
+        int lafel=0;
+        calculeazaEcuatia(a[i*2],a[i*2+1],b[i*2],b[i*2+1],ec[i].panta,ec[i].terlib,ec[i].coefy,lafel);
+
+        laFel[i]=lafel;
+
+        distante[i]=float(a[2*i]-b[2*i])/(float)100;
+
+        if(laFel[i]==2)
+            distante[i]=float(a[2*i+1]-b[2*i+1])/(float)100;
+
+        if(distante[i]<0)distante[i]*=-1;
+        cout<<" dis:"<<distante[i]<<' ';
+
+        if(laFel[i]==2)
+            if((b[i*2+1]-a[i*2+1])<0)
+                susjos[i]=1;
+
+        if(laFel[i]==3)
+            if((b[i*2]-a[i*2])>0)
+                stdr[i]=1;
+
+        if(laFel[i]==4)
+        {
+            if((b[i*2+1]-a[i*2+1])<0)
+                susjos[i]=1;
+            if((b[i*2]-a[i*2])>0)
+                stdr[i]=1;
+        }
+        cout<<" stdr:"<<stdr[i]<<' '<<" susjos:"<<susjos[i];
     }
+
+    float aux[2*n]= {0};
+    int auxint[2*n]= {0};
+    for(int i=1; i<n; i++)
+        aux[i]=a[i];
+    drawpoly(n,aint);
+    delay(700);
+
+    for(int i=1; i<=100; i++)
+    {
+        setfillstyle(SOLID_FILL,BLACK);
+        for(int j=0; j<n; j++)
+        {
+            if(laFel[i]==2)
+            {
+                if(susjos[i]>1)
+                    aux[j*2+1]=aux[j*2+1]-distante[j];
+                else
+                    aux[j*2+1]=aux[j*2+1]+distante[j];
+            }
+            if(laFel[i]==3)
+            {
+                if(stdr[i]>1)
+                    aux[j*2+1]=aux[j*2+1]+distante[j];
+                else
+                    aux[j*2+1]=aux[j*2+1]-distante[j];
+            }
+            if(laFel[i]==4)
+            {
+                if(stdr[i]>0)
+                {
+                    aux[j*2]=aux[j*2]+distante[j];
+                    aux[j*2+1]=-(ec[i].panta*aux[j*2]+ec[i].terlib);
+                }
+                else
+                {
+                    aux[j*2]=aux[j*2]-distante[j];
+                    aux[j*2+1]=-(ec[i].panta*aux[j*2]+ec[i].terlib);
+                }
+            }
+        }
+        for(int k=0; k<2*n; k++)
+            auxint[k]=(int)aux[k];
+      //  bar(2,2,698,698);
+        drawpoly(n,auxint);
+        delay(30);
+
+
+    }
+   // drawpoly(n,bint);
+}
+void culoareoriginala(int c)  // folosit pt aducere butoanele 1 si 2 la normal
+{
+    setfillstyle(SOLID_FILL,LIGHTGREEN);
+    bar(B[c].D.SS.x, B[c].D.SS.y, B[c].D.DJ.x, B[c].D.DJ.y);
+    setcolor(WHITE);
+    setbkcolor(BLACK);
+    outtextxy(B[c].D.SS.x+10,B[c].D.SS.y+15,B[c].text);
+    setcolor(GREEN);
+    rectangle(B[c].D.SS.x, B[c].D.SS.y,B[c].D.DJ.x,B[c].D.DJ.y);
+    setcolor(WHITE);
+}
 
 
 int main()
@@ -206,48 +312,12 @@ int main()
 
     initwindow(1400,700);
     deseneazaMeniul();
-
-    for(int i=0; i<14; i++)
+    for(int i=1; i<=8; i++)
+        for(int j=0; j<50; j++)
         {
-        fin>>trapez[i];
-        trapezint[i]=trapez[i];
+            fin>>img[i][j];
+            imgint[i][j]=img[i][j];
         }
-    for(int i=0; i<14; i++)
-        {
-        fin>>casa[i];
-        casaint[i]=casa[i];
-        }
-
-    for(int i=0; i<7; i++) ///for ul asta imi calculeaza mie o gramada de chestii n ai trb cu el
-    {
-        int lafel=0;
-        calculeazaEcuatia(trapez[i*2],trapez[i*2+1],casa[i*2],casa[i*2+1],ec[i].panta,ec[i].terlib,ec[i].coefy,lafel);
-
-        if(lafel==1)
-        {
-            afisecuatii<<"fara"<<'\n';
-            laFel[i]=1;
-        }
-        else
-        {
-            afisecuatii<<"0="<<ec[i].coefy<<"y+"<<ec[i].panta<<"x+"<<ec[i].terlib<<endl;
-            laFel[i]=lafel;
-        }
-
-        distante[i]=float(trapez[2*i]-casa[2*i])/100;
-        if(laFel[i]==2)
-            distante[i]=float(trapez[2*i+1]-casa[2*i+1])/100;
-        if(distante[i]<0)distante[i]*=-1;
-        cout<<distante[i]<<' ';
-
-        if(laFel[i]==3||laFel[i]==4)
-            if((casa[i*2]-trapez[i*2])>0)
-                stdr[i]=1;
-        if(laFel[i]==2)
-            if((casa[i*2+1]-trapez[i*2+1])>0)
-                stdr[i]=1;
-
-    }
 
     /// ----------------------------------------------------------------------
 
@@ -265,7 +335,8 @@ int main()
             comanda=butonul_apasat;
             cout<<"Comanda "<<comanda<<endl; // retine comenzile folosite in compiler
 
-            if(butonul_apasat==1 && lockbut==0){
+            if(butonul_apasat==1 && lockbut==0)
+            {
                 enableimg1=1;
                 enableimg2=0;
 
@@ -277,9 +348,10 @@ int main()
                 outtextxy(B[1].D.SS.x+10,B[1].D.SS.y+15,B[1].text);
 
                 culoareoriginala(2);
-                }
+            }
 
-            if(butonul_apasat==2 && lockbut==0){
+            if(butonul_apasat==2 && lockbut==0)
+            {
                 enableimg1=0;
                 enableimg2=1;
 
@@ -294,94 +366,46 @@ int main()
             }
 
             if(butonul_apasat==3 && lockbut==0) /// ALG TREBUIE INLOCUIT CU UN VOID START(IMG1, IMG2) !!! PLUS ATENTIE LA CAZUL IN CARE NU AU FOST ALESE IMG INCA (PREV1 SAU PREV2 = 0)
-            {   culoareoriginala(1);
+            {
+                culoareoriginala(1);
                 culoareoriginala(2);
 
                 setviewport(700,80, 700,1400, 0);
                 setcolor(WHITE);
                 lockbut=1;
-
-                //start(img1,img2)}
-
-                drawpoly(7,casaint);
-                delay(700);
-
-                /* while(trapez[8]!=casa[8])  // while -> subprogram (cord img1, cord img 2)
-                 {
-                     delay(500);
-                     clearviewport();
-                     casa[8]=casa[8]-30; //diferenta calculata in notepad -> algoritm de calcul
-                     casa[10]=casa[10]+28;
-                     casa[5]=casa[5]+35;
-                     drawpoly(7,casa);
-                 } */
-                 ///MERGE!!!!! bag pl in el algoritm e ora 3 dimineata nb
-
-                for(int i=1; i<=100; i++)
-                {
-                    setfillstyle(SOLID_FILL,BLACK);
-                    bar(2,2,698,698);
-
-                    for(int j=0; j<7; j++)
-                    {
-                        if(stdr[j]!=1)
-                        {
-                            if(laFel[j]==2)
-                                casa[j*2+1]=casa[j*2+1]+distante[j];
-                            if(laFel[j]==3)
-                                casa[j*2]=casa[j*2]+distante[j];
-                            if(laFel[j]==4)
-                            {
-                                casa[j*2]=casa[j*2]+distante[j];
-                                casa[j*2+1]=casa[j*2]*ec[j].panta+ec[j].terlib;
-                            }
-                        }
-                        else
-                        {
-                            if(laFel[j]==2)
-                                casa[j*2+1]=casa[j*2+1]-distante[j];
-                            if(laFel[j]==3)
-                                casa[j*2]=casa[j*2]-distante[j];
-                            if(laFel[j]==4)
-                            {
-                                casa[j*2]=casa[j*2]-distante[j];
-                                casa[j*2+1]=casa[j*2]*ec[j].panta+ec[j].terlib;
-                            }
-                        }
-                    }
-                    for(int k=0;k<15;k++)
-                        casaint[k]=casa[k];
-                    drawpoly(7,casaint);
-                    delay(1);
-                }
-                clearviewport();
-                drawpoly(7,trapezint);
-
+                ///---------
+                morph(img[prev1-4],img[prev2-4],imgint[prev1-4],imgint[prev2-4],25);
+                ///---------
                 enableimg1=0;
                 enableimg2=0;
             }
 
-            if(butonul_apasat > 3){ //schimbarea culorii butonului imagine selectat si retinerea in prev1 si prev 2
+            if(butonul_apasat > 3)  //schimbarea culorii butonului imagine selectat si retinerea in prev1 si prev 2
+            {
 
-                if(enableimg1==1 && butonul_apasat!=prev2){
-                    if(prev1!=0){
+                if(enableimg1==1 && butonul_apasat!=prev2)
+                {
+                    if(prev1!=0)
+                    {
                         setcolor(WHITE);
                         rectangle(B[prev1].D.SS.x, B[prev1].D.SS.y, B[prev1].D.DJ.x, B[prev1].D.DJ.y);
                     }
                     setcolor(RED);
                     imag=butonul_apasat;
-                        rectangle(B[imag].D.SS.x, B[imag].D.SS.y, B[imag].D.DJ.x, B[imag].D.DJ.y);
+                    rectangle(B[imag].D.SS.x, B[imag].D.SS.y, B[imag].D.DJ.x, B[imag].D.DJ.y);
                     prev1=imag;
                 }//prev1 ramane valoarea imaginei 1 aleasa
 
-                if(enableimg2==1 && butonul_apasat!=prev1){
-                    if(prev2!=0){
+                if(enableimg2==1 && butonul_apasat!=prev1)
+                {
+                    if(prev2!=0)
+                    {
                         setcolor(WHITE);
                         rectangle(B[prev2].D.SS.x, B[prev2].D.SS.y, B[prev2].D.DJ.x, B[prev2].D.DJ.y);
                     }
                     setcolor(GREEN);
                     imag=butonul_apasat;
-                        rectangle(B[imag].D.SS.x, B[imag].D.SS.y, B[imag].D.DJ.x, B[imag].D.DJ.y);
+                    rectangle(B[imag].D.SS.x, B[imag].D.SS.y, B[imag].D.DJ.x, B[imag].D.DJ.y);
                     prev2=imag;
                 }//prev2 ramane valoarea imaginiei 2 aleasa --. de folosir in functie start(prev1, prev2)
 
@@ -398,7 +422,5 @@ int main()
 Albert:
 ce mai am de facut:
 trebuie sa bag tot algoritmul de morphing intr o functie
-sa folosesc structura punct in loc de inlantuirea vectorului, totusi nu cred pot pt ca drawpoly foloseste vector ca sa construiasca linia
-sa fie imaginile formate din mai multe linii poligonale
 sa se poata morfa din oricare imagine in oricare alta imagine din cele 3 (asata trebuie vazut si cu partea de frontend)
 */
